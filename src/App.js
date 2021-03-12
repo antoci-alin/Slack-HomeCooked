45f345593a23cb30032a8b68a043893dcca48d51
Alin Georgian, Antoci <alin.georgian.antoci@gmail.com>
2021-03-12 17:21:26 +0200

    Project set
    - react, redux, firebase cfg, styled components & material

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './Header';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <>
+          <Switch>
+            <Route path="/" exact>
+              <Header />
+            </Route>
+          </Switch>
+        </>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAHFwIIOiWw9YYCcNAfMWbcHOzEv0jFu6Y',
+  authDomain: 'slack-home-cooked.firebaseapp.com',
+  projectId: 'slack-home-cooked',
+  storageBucket: 'slack-home-cooked.appspot.com',
+  messagingSenderId: '697783253388',
+  appId: '1:697783253388:web:9e5a7c92aa85482f8f7ea9',
+};
